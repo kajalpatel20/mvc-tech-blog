1ca45d70f96e3e4fb2c848be6ec12bf695f39ebd
Kajal Patel <kajalpatel20@gmail.com>
2021-04-05 23:24:10 -0400

    fix error

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -4,7 +4,7 @@ async function newFormHandler(event) {
     const title = document.querySelector('input[name="post-title"]').value;
     const post_content = document.querySelector('input[name="post-content"]').value;
   
-    const response = await fetch(`/api/posts`, {
+    const response = await fetch('/api/posts', {
       method: 'POST',
       body: JSON.stringify({
         title,
@@ -22,4 +22,4 @@ async function newFormHandler(event) {
     }
   }
   
-  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler)
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
